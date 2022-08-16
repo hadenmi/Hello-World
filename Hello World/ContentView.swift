af0ff3e27d395badfcc6e013c7af6309b8292463
Haden Miller <s0541780@student.rockvalleycollege.edu>
2022-08-15 20:24:50 -0500

    Initial Commit

---
+++ Hello World/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello World
+//
+//  Created by Real mac user on 8/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Haden!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Real mac user on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
